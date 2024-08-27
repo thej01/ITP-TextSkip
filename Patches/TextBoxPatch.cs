d60e576ac4c5b333559c71894d3ca77ddfa0d49e
thej01 <152479044+thej01@users.noreply.github.com>
2024-08-27 12:33:00 -0700

    v1.0.0

---
+++ Patches/TextBoxPatch.cs
@@ -0,0 +1,81 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static ITP_TextSkip.TextSkip;
+
+namespace ITP_TextSkip.Patches
+{
+
+    [HarmonyPatch(typeof(TextBox))]
+    internal class TextBoxPatch
+    {
+
+        public static bool Registered = false;
+        public static bool AudioInitalized = false;
+
+        public static TextBox Txtbox = null;
+
+        public static bool TxtboxActive()
+        {
+            if (Txtbox == null)
+                return false;
+
+            if (!Txtbox.IsDialogueActive())
+                return false;
+
+            return true;
+        }
+
+        [HarmonyPatch("Awake")]
+        [HarmonyPostfix]
+        public static void PostAwake(ref TextBox __instance)
+        {
+            if (!Registered)
+            {
+                GameInput.Register(GameInput.KeyMap.Run, 8000, new Func<InputAction.CallbackContext, bool>(OnSkipInput));
+
+                Registered = true;
+                logger.Log("Registed run input for txt skip");
+            }
+
+            TextBox.AutoSkipDialogues = false;
+            Txtbox = __instance;
+        }
+
+        public static bool OnSkipInput(InputAction.CallbackContext ctx)
+        {
+            if (!TxtboxActive())
+            {
+                TextBox.AutoSkipDialogues = false;
+                return false;
+            }
+
+            if (ctx.started)
+            {
+                logger.Log("Skipping!");
+                TextBox.AutoSkipDialogues = true;
+            }
+            else if (ctx.canceled)
+            {
+                logger.Log("Stopped skipping!");
+                TextBox.AutoSkipDialogues = false;
+            }
+
+            // Skips the dialogue
+            if (TextBox.AutoSkipDialogues)
+            {
+                TextBox.SkipTypewriter();
+                if (!Txtbox.DisplayNext())
+                {
+                    Txtbox.Hide();
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ITP_TextSkip.Patches;
+
+namespace ITP_TextSkip
+{
+    public class Logger
+    {
+        internal ManualLogSource MLS;
+
+        private string modGUID = null;
+        private string modName = null;
+        private string modVersion = null;
+
+        public void Init(string _modName, string _modVersion, string _modGUID)
+        {
+            modGUID = _modGUID;
+            modName = _modName;
+            modVersion = _modVersion;
+
+            MLS = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+        }
+
+        public void Log(string text = "", LogLevel level = LogLevel.Info)
+        {
+            string resultText = string.Format("[{0} v{1}] - {2}", modName, modVersion, text);
+
+            MLS.Log(level, resultText);
+        }
+    }
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class TextSkip : BaseUnityPlugin
+    {
+        private const string modGUID = "thej01.itp.TextSkip";
+        private const string modName = "Text Skip";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static TextSkip Instance;
+
+        public static Logger logger = new Logger();
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            logger.Init(modName, modVersion, modGUID);
+
+            logger.Log("logger Initialised!");
+
+            logger.Log("Patching TextSkip...");
+            harmony.PatchAll(typeof(TextSkip));
+            logger.Log("Patched TextSkip.");
+
+            logger.Log("Patching TextBoxPatch...");
+            harmony.PatchAll(typeof(TextBoxPatch));
+            logger.Log("Patched TextBoxPatch.");
+        }
+    }
+}
